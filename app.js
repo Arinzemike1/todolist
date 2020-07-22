7679d0e957ab6eb1b4414ed0f207732e6e132a86
Arinze Ihemedu <arinze.ihemedu@konga.local>
2020-07-22 22:17:40 +0100

    Modify all files

--- app.js
+++ app.js
@@ -1,55 +1,39 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("li");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("span");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+function newItem() {
+  let input = document.getElementById("input");
+  let ul = document.getElementById("ul");
+  let li = document.createElement("li");
+
+
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  // document.getElementById("input").value =  "";
+  input.value = "";
+  li.onclick = removeItem;
 }
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
+document.body.onkeyup = function(event) {
+  if (event.keyCode === 13) {
+    newItem();
   }
-}
+};
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+let syncBtn = document.getElementById("syncBtn");
+syncBtn.addEventListener("click", function(){
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {  
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);  
+  let values = ul.appendChild(li)
+  let allValues = {}
+  allValues[values]
+  console.log(allValues);
+  axios.post('https://jsonplaceholder.typicode.com/users', {
+    allValues
+  }).then(response => {
+    console.log(response);
+  });
+})
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+function removeItem(event){
+  event.target.parentElement.removeChild(event.target);
 }
